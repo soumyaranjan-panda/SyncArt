434dee3a496734f7119ea436977cf1d93d5976cd
SoumyaPanda <soumyaranjanpanda988@gmail.com>
2024-11-16 21:09:31 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const { Server } = require("socket.io");
 const cors = require('cors');
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 const server = http.createServer(app);
 const io = new Server(server, {
